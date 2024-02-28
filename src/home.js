db0cef3bd3464e85ea82a515f218f0672e852794
ManyuDhyani <manyudhyani@gmail.com>
2024-02-28 16:48:42 -0500

    Event button

--- src/home.js
+++ src/home.js
@@ -3,11 +3,15 @@ const Home = () => {
     const handleClick = () => {
         console.log('hello, ninjas');
     }
+    const handleClickAgain = (name, e) => {
+        console.log("Second button" + name, e)
+    }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => {handleClickAgain('manyu', e)}}> Click me Again </button>
         </div>
       );
 }
