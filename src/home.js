f3aa244a99db8c1d287154dd04aecc90803c4c8c
ManyuDhyani <manyudhyani@gmail.com>
2024-03-01 01:37:01 -0500

    Componets arrangement

--- src/home.js
+++ src/home.js
@@ -1,26 +1,46 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-    //et name = 'mario';
-
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-    const handleClick = () => {
-        setName("Manyu")
-        setAge(28)
-    }
-    // const handleClickAgain = (name, e) => {
-    //     console.log("Second button" + name, e)
-    // }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
 
     return (
-        <div className="home">
-            <h2>Homepage</h2> 
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click me</button>
-            {/* <button onClick={(e) => {handleClickAgain('manyu', e)}}> Click me Again </button> */}
+        <div className='home'>
+            <BlogList blogs={blogs} title="All Blogs!" />
+            <BlogList blogs={blogs.filter((blog)=> blog.author === "mario")} title="Mario's Blog" />
         </div>
-      );
+    )
 }
  
 export default Home;
+
+
+
+
+
+// const Home = () => {
+//     //et name = 'mario';
+
+//     const [name, setName] = useState('mario');
+//     const [age, setAge] = useState(25);
+//     const handleClick = () => {
+//         setName("Manyu")
+//         setAge(28)
+//     }
+//     // const handleClickAgain = (name, e) => {
+//     //     console.log("Second button" + name, e)
+//     // }
+
+//     return (
+//         <div className="home">
+//             <h2>Homepage</h2> 
+//             <p>{name} is {age} years old</p>
+//             <button onClick={handleClick}>Click me</button>
+//             {/* <button onClick={(e) => {handleClickAgain('manyu', e)}}> Click me Again </button> */}
+//         </div>
+//       );
+// }
