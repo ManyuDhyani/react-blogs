1469324d89c2d1c280de13f900d56043978d8ae8
ManyuDhyani <manyudhyani@gmail.com>
2024-03-01 23:25:58 -0500

    Created the json server

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -8,15 +8,23 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
       ])
 
+    const [name, setName] = useState('mario')
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        console.log("Use effect ran")
+    }, [name]);
+
     return (
         <div className='home'>
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
             <BlogList blogs={blogs.filter((blog)=> blog.author === "mario")} title="Mario's Blog" />
+            <button onClick={()=>setName('Manyu')}>Click here</button>
+            <p>{name}</p>
         </div>
     )
 }
